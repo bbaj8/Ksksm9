ba9140e72abacf9028d5e6f4f9a8e924063f70dd
WaterySaucer113pp <93452656+bbaj8@users.noreply.github.com>
2022-03-16 01:17:23 +0300

    Delete plugins directory

--- plugins/start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone https://github.com/bbaj8/Ksksm9.git /ksksm9
-echo "Installing Requirements..."
-cd /ksksm9
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 main.py
